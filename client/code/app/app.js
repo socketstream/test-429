efce22ef8e36374e668265f4a87a19b08f358a9c
Paul Jensen <paulbjensen@gmail.com>
2014-04-16 05:52:17 +0100

    I think I've replicated the bug

--- client/code/app/app.js
+++ client/code/app/app.js
@@ -1,17 +1,22 @@
-// Subscribe to channel
-//
-ss.rpc('pubsubissue.subscribeToChannel', 'arbChannel', console.log);
+'use strict';
 
+
+
+// Subscribe to channel if running on port 3001
+//
+if (document.location.origin.match(3001) !== null) {
+    ss.rpc('pubsubissue.subscribeToChannel', 'arbChannel');
+}
 
 
 // Log out any messages on that channel
 //
 ss.event.on('arbs:update', function (message, channelName) {
-  console.log(channelName, 'received this data:', message);
+    console.log(channelName, 'received this data:', message);
 });
 
 
 
-setTimeout(function() {
-  ss.rpc('pubsubissue.publishMessage', 'arbs:update', 'hello world', console.log);
-}, 10000);
+setInterval(function() {
+    ss.rpc('pubsubissue.publishMessage', 'arbs:update', 'hello world');
+}, 5000);
